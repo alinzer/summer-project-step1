ef5c51cbbd72845d89cc23052b8e77ae39a3c986
MarkGotesman <markgotesman711@gmail.com>
2022-06-29 17:11:35 -0400

    Search-Simplification-Cleanedup

--- src/main/java/edu/yu/cs/artAPI/Art.java
+++ src/main/java/edu/yu/cs/artAPI/Art.java
@@ -4,25 +4,10 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import org.hibernate.annotations.Filter;
-import org.hibernate.annotations.FilterDef;
-import org.hibernate.annotations.ParamDef;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-@FilterDef (
-    name = "search", 
-        parameters = {
-            @ParamDef(name = "galleryId", type = "long"),
-            @ParamDef(name = "name", type = "string"),
-            @ParamDef(name = "creator", type = "string")
-        },
-    defaultCondition = "gallery_id=:galleryId and name=:name and creator=:creator"
-)
-
-@Filter (name = "search")
 @Entity
 public class Art extends PanacheEntity {
     public String name;
